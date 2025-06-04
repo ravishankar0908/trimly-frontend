935b7e823b1fdc0e9e261e57b9db8d3c01963bb5
ravishankar0908 <ravishankar007cbi@gmail.com>
2025-06-04 15:18:28 +0530

    registration page is created.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,28 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatCheckboxModule,
+  ],
 })
 export class AuthModule {}
--- src/app/auth/components/registration/registration.component.ts
+++ src/app/auth/components/registration/registration.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
+  passwordHide: Boolean = true;
+  confirmPasswordHide: Boolean = true;
+  registrationFormData!: FormGroup;
 
+  ngOnInit(): void {
+    this.getFormData();
+  }
+
+  getFormData() {
+    this.registrationFormData = this.formBuilder.group(
+      {
+        firstName: ['', [Validators.required]],
+        lastName: ['', [Validators.required]],
+        gender: ['', [Validators.required]],
+        city: ['', [Validators.required]],
+        phoneNumber: [
+          '',
+          [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)],
+        ],
+        emailAddress: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(/^(?=.*[A-Z])(?=.*[^a-zA-Z0-9])/),
+            Validators.minLength(8),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+        role: ['user', [Validators.required]],
+        termAndConditions: [true, Validators.requiredTrue],
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
+
+  Submit() {
+    if (this.registrationFormData.invalid) {
+      this.registrationFormData.markAllAsTouched();
+      return;
+    } else {
+      console.log(this.registrationFormData.value);
+      this.getFormData();
+    }
+  }
+
+  passwordMatchValidator(formGroup: AbstractControl) {
+    const password = formGroup.get('password');
+    const confirmPassword = formGroup.get('confirmPassword');
+    if (password?.value !== confirmPassword?.value) {
+      return confirmPassword?.setErrors({ passwordMismatch: true });
+    } else {
+      const errors = confirmPassword?.errors;
+      if (errors) {
+        delete errors['passwordMismatch'];
+        if (Object.keys(errors).length === 0) {
+          confirmPassword?.setErrors(null);
+        } else {
+          confirmPassword?.setErrors(errors);
+        }
+      }
+    }
+    return null;
+  }
+
+  signInWithGoogle() {
+    alert('sign with google');
+  }
 }
