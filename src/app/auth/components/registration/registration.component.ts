27bc26deb12833a904a86b25ad56be0c6b4588d9
ravishankar0908 <ravishankar007cbi@gmail.com>
2025-06-09 14:06:35 +0530

    small changes has been made in regitration comp

--- src/app/auth/components/registration/registration.component.ts
+++ src/app/auth/components/registration/registration.component.ts
@@ -25,7 +25,7 @@ export class RegistrationComponent implements OnInit {
   passwordHide: boolean = true;
   confirmPasswordHide: boolean = true;
   user: boolean = true;
-
+  isSubmitting: boolean = false;
   registrationFormData!: FormGroup;
 
   ngOnInit(): void {
@@ -124,7 +124,8 @@ export class RegistrationComponent implements OnInit {
       this.registrationFormData.markAllAsTouched();
       return;
     } else {
-      // implement your service calling here
+      this.isSubmitting = true;
+
       this.authService
         .Registration(this.registrationFormData.value as UserRegistrationModel)
         .subscribe(
@@ -132,15 +133,21 @@ export class RegistrationComponent implements OnInit {
             this.toaster.success(res.message, 'success');
             this.registrationFormData.reset();
             this.registrationFormData.markAsUntouched();
+            this.registrationFormData.markAsPristine();
+
+            Object.keys(this.registrationFormData.controls).forEach((key) => {
+              this.registrationFormData.get(key)?.setErrors(null);
+            });
 
             this.registrationFormData.patchValue({
               role: 'user',
               termAndConditions: true,
             });
-
-            //this.router.navigate(['auth/login']);
+            this.isSubmitting = false;
+            this.router.navigate(['auth/login']);
           },
           (err) => {
+            this.isSubmitting = false;
             if (err.status === 409)
               this.toaster.error(err.error.messages, 'error');
             if (err.status === 406)
