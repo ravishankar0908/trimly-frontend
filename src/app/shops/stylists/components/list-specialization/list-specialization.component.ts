af79f6b8ec1a31def384c04f183b072d9d7735eb
ravishankar0908 <ravishankar007cbi@gmail.com>
2025-07-09 21:09:54 +0530

    stylist's specialization is implemented

--- src/app/shops/stylists/components/list-specialization/list-specialization.component.ts
+++ src/app/shops/stylists/components/list-specialization/list-specialization.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpecializationService } from '../../service/specialization.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-list-specialization',
   templateUrl: './list-specialization.component.html',
-  styleUrls: ['./list-specialization.component.scss']
+  styleUrls: ['./list-specialization.component.scss'],
 })
-export class ListSpecializationComponent {
+export class ListSpecializationComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'description', 'action'];
+  dataSource: any[] = [];
+  isEmptyData: boolean = true;
+  itemsPerPage: number = 5;
+  pageNumber: number = 1;
+  pageLength: number = 0;
+  constructor(private specializationService: SpecializationService) {}
 
+  ngOnInit(): void {
+    this.getAllSpecialization();
+  }
+
+  private getAllSpecialization() {
+    this.specializationService
+      .getSpecialization(this.itemsPerPage, this.pageNumber)
+      .subscribe({
+        next: (res) => {
+          this.handleSuccess(res);
+        },
+        error: (err) => {},
+      });
+  }
+
+  private handleSuccess(res: any) {
+    this.dataSource = res.data;
+    this.pageLength = res.totalCount;
+
+    if (res.data.length === 0) {
+      this.isEmptyData = true;
+    } else {
+      this.isEmptyData = false;
+    }
+  }
+
+  refreshData() {
+    this.getAllSpecialization();
+  }
+
+  handlePaginator(event: PageEvent) {
+    this.itemsPerPage = event.pageSize;
+    this.pageNumber = event.pageIndex + 1;
+    this.specializationService
+      .getSpecialization(this.itemsPerPage, this.pageNumber)
+      .subscribe({
+        next: (res) => {
+          this.handleSuccess(res);
+        },
+        error: (err) => {},
+      });
+  }
 }
--- src/app/shops/stylists/service/specialization.service.ts
+++ src/app/shops/stylists/service/specialization.service.ts
@@ -1,19 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpecializationService {
   constructor(private http: HttpClient) {}
-  api = 'http://localhost:3000/specialization/add';
+  api = 'http://localhost:3000/specialization';
 
+  id = localStorage.getItem('id');
   insertSpecialization(data: any): Observable<any> {
-    return this.http.post(
-      `${this.api}?userId=${localStorage.getItem('id')}`,
-      data,
-      { withCredentials: true }
+    return this.http.post(`${this.api}/add?userId=${this.id}`, data, {
+      withCredentials: true,
+    });
+  }
+
+  getSpecialization(itemsPerPage: number, pageNumber: number): Observable<any> {
+    return this.http.get(
+      `${this.api}/?userId=${this.id}&pageNumber=${pageNumber}&itemsPerPage=${itemsPerPage}`,
+      {
+        withCredentials: true,
+      }
     );
+  }
+
+  getListSpecialization(): Observable<any> {
+    return this.http.get(`${this.api}/?userId=${this.id}`, {
+      withCredentials: true,
+    });
   }
 }
